3f950c997ba47f19bb181beeb13c466116511460
Mark Rademaker <mark-rademaker@hotmail.nl>
2015-11-11 10:34:33 +0100

    Laat je eigen posts zien op Your Post pagina

--- resources/views/user/posts.blade.php
+++ resources/views/user/posts.blade.php
@@ -6,18 +6,22 @@
     <li><a href="/">Home</a></li>
     <li><a href="form/create">Create</a></li>
     <li><a href="user/form">Your Posts</a></li>
-    <li><a href="auth/logout">logout</a></li>
+    <li><a href="auth/logout">Logout</a></li>
     <li><a href="form">Pictures</a></li>
 </ul>
 <hr>
 <div>
-	<h4>{{ $forms->title }}</h4>
-	<article>
-		{{ $forms->content }}
-	</article>
-	<hr>
+	@foreach ($forms as $form)
+		@if (\Auth::user()->id == $form->user_id)
+			<h4>{{ $form->title }}</h4>
+			<article>
+				{{ $form->content }}
+			</article>
+			<a href="{{ url('/form/edit', $form->id) }}"><p>Change form</p></a>
+			<hr>
+		@endif
+	@endforeach
 </div>
 <a href="/form">Go back</a>
-<a href="{{ url('/form/edit', $forms->id) }}"><p>Change form</p></a>
 </div>
 @stop
