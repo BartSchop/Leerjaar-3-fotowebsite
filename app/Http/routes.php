edb48fba7206c75d326326f3cebb6b0226132754
Mark Rademaker <mark-rademaker@hotmail.nl>
2015-11-24 11:38:04 +0100

    Fully working reset password function

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -59,3 +59,4 @@ Route::post('password/email', 'Auth\PasswordController@postEmail');
 // Password reset routes...
 Route::get('password/reset/{token}', 'Auth\PasswordController@getReset');
 Route::post('password/reset', 'Auth\PasswordController@postReset');
+Route::get('home', 'FormController@index');
